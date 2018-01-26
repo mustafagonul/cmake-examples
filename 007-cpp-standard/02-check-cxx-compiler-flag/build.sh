164221c435f6eccdcecbfffb84d056e8d50d11be
Mustafa Gonul <mustafagonul@gmail.com>
2018-01-26 16:01:01 +0300

    - 007-cpp-standard / 02-check-cxx-compiler-flag

---
+++ 007-cpp-standard/02-check-cxx-compiler-flag/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm -Rf build
+rm -Rf install
+
+mkdir build
+mkdir install
+
+cd build
+
+# Default: /usr/local
+# cmake ..
+
+# Custom: ../install
+cmake .. -DCMAKE_INSTALL_PREFIX=../install
+
+## Verbose on
+# cmake .. -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+make VERBOSE=1
+
+make
+make install
